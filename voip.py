fe27791f8c1545fbdd34cdeb7d8bf44c2cd03251
Trishant Pahwa <trishantpahwa@gmail.com>
2020-06-07 13:18:12 +0530

    Changed library to PyAudio

--- voip.py
+++ voip.py
@@ -1,17 +1,38 @@
+import wave
+import pyaudio
+import time
 import paho.mqtt.client as mqtt
-import sounddevice as sd
-import numpy as np
-import scipy.io.wavfile as wav
 
+chunk = 1024  # Record in chunks of 1024 samples
+sample_format = pyaudio.paInt16  # 16 bits per sample
+channels = 2
+fs = 21500  # Record at 44100 samples per second
+
+p = pyaudio.PyAudio()  # Create an interface to PortAudio
+
+print('Recording')
 client = mqtt.Client()
 client.connect("192.168.1.33", 1883, 60)
 
-while True:
-    fs=20100
-    duration = 1  # seconds
-    myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=2,dtype='float64')
-    print("Recording Audio")
-    sd.wait()
-    myrecording = myrecording.tobytes()
-    client.publish("voip", myrecording)
-    
+def callback(in_data, frame_count, time_info, status):
+    print("Published")
+    client.publish("voip", in_data)
+    return (None, pyaudio.paContinue)
+
+stream = p.open(format=sample_format,
+                channels=channels,
+                rate=fs,
+                frames_per_buffer=chunk,
+                input=True,stream_callback=callback)
+
+# Stop and close the stream
+stream.start_stream()
+while stream.is_active():
+    time.sleep(0.1)
+stream.stop_stream()
+stream.close()
+# Terminate the PortAudio interface
+p.terminate()
+
+print('Finished recording')
+
--- voip_client.py
+++ voip_client.py
@@ -1,17 +1,33 @@
 import paho.mqtt.client as mqtt
-import sounddevice as sd
-import numpy as np
-import scipy.io.wavfile as wav
+import pyaudio
+import time
+
+
+chunk = 1024  # Record in chunks of 1024 samples
+sample_format = pyaudio.paInt16  # 16 bits per sample
+channels = 1
+fs = 44100
+p = pyaudio.PyAudio()
+stream = p.open(format=sample_format,
+                channels=channels,
+                rate=fs,
+                frames_per_buffer=chunk,
+                output=True)
 
 def on_message(client, userdata, msg):
-    fs=44100
     print(msg.topic+" ")
-    msg = np.frombuffer(msg.payload)
-    sd.play(msg, fs)
-    sd.wait()
+    stream.write(msg.payload)
+
 
 client = mqtt.Client()
-client.connect("192.168.1.28", 1883, 60)
+client.connect("192.168.1.33", 1883, 60)
 client.subscribe("voip")
 client.on_message = on_message
 client.loop_forever()
+stream.start_stream()
+while stream.is_active():
+    time.sleep(0.1)
+stream.stop_stream()
+stream.close()
+# Terminate the PortAudio interface
+p.terminate()
