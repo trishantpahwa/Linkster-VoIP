51b44353306f0928fcd6c0f8d82f4527d8dfd9a7
Trishant Pahwa <trishantpahwa@gmail.com>
2020-07-22 16:48:08 +0530

    Update.

--- voip_input.js
+++ voip_input.js
@@ -1,4 +1,3 @@
-var audioCtx = new AudioContext();
 var constraints = {
     audio: true,
     video: true
@@ -10,46 +9,61 @@ client.on('connect', function () {
 });
 client.on('message', function(topic, payload) {
     console.log(topic);
+    // buffer = payload;
     // console.log(payload);
     var buffer = new Blob([payload], {type: "video/x-matroska;codecs=avc1,opus"}); // audio/webm;codecs=opus video/x-matroska;codecs=avc1,opus
-    var url = URL.createObjectURL(buffer);
+    // buffer = b;
     // var a = new Audio();
     // a.autoplay = true;
     // a.src = url;
     // a.load();
-    var v = document.getElementById("myvideo");
-    v.src = url;
+    var remoteVideo = document.getElementById("remoteVideo");
+    var url = URL.createObjectURL(buffer);
+    remoteVideo.src = url;
+    // v.src = url;
 });
 $(document).ready(function() {
     navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-        var chunks = [];
+        var localVideo = document.getElementById('localVideo');
+        var remoteVideo = document.getElementById('remoteVideo');
+        var buffer = null;
+        var codec = "video/webm;codecs=opus";
+        var mediaSource = new MediaSource();
+        // var sourceBuffer = mediaSource.addSourceBuffer(codec);
+        mediaSource.onsourceopen = function(t) {
+            console.log(t);
+            console.log('Hello');
+        };
+        // console.log(sourceBuffer);
+        // var buffer = new Blob([], { type: "video/x-matroska;codecs=avc1,opus" });
+        // var url = URL.createObjectURL(buffer);
+        // remoteVideo.src = url;
+        localVideo.srcObject = stream;
+        // var chunks = [];
         // var v = document.getElementById("myvideo");
         // v.srcObject = stream;
         // v.play();
-        var source = audioCtx.createMediaStreamSource(stream);
-        // const mediaRecorder = new MediaRecorder(stream);
-
-        // mediaRecorder.addEventListener("dataavailable", event => {
-        //     // console.log(event.data);
-        //     audioChunks = event.data;
-        //     // event.data.arrayBuffer().then(function(buffer) {
-        //     //     buffer = new Uint8Array(buffer);
-        //     //     client.publish("voip1", buffer);
-        //     // });
-        // });
+        const mediaRecorder = new MediaRecorder(stream);
+        mediaRecorder.addEventListener("dataavailable", event => {
+            // console.log(event.data);
+            audioChunks = event.data;
+            event.data.arrayBuffer().then(function(buffer) {
+                buffer = new Uint8Array(buffer);
+                client.publish("voip", buffer);
+            });
+        });
         // mediaRecorder.onstop = function(event) {
         //     console.log(event);
         // };
                 
-        // function rec() {
-        //     mediaRecorder.start();
-        //     setTimeout(function() {
-        //         mediaRecorder.stop();
-        //         rec();
-        //     }, 2000);
-                        
-        // }
-        // rec();
+        function rec() {
+            mediaRecorder.start();
+            setTimeout(function() {
+                mediaRecorder.stop();
+                rec();
+            }, 2000);                
+        }
+        rec();
         // mediaRecorder.ondataavailable = function(event) {
             // event.data.arrayBuffer().then(function(buffer) {
                 // buffer = new Uint8Array(buffer);
