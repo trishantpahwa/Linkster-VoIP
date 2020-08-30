89291b32eb04656765b8f5f0998d2e2a58859853
Trishant Pahwa <trishantpahwa@gmail.com>
2020-08-30 17:05:05 +0530

    Updated code.

---
+++ public/script.js
@@ -0,0 +1,55 @@
+const socket = io('/');
+const videoGrid = document.getElementById('video-grid');
+const myPeer = new Peer(undefined, {
+    host: '/herokuman.com',
+    port: '3001',
+    secure: true
+});
+
+const peers = { };
+
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+navigator.mediaDevices.getUserMedia({video: true, audio: true}).then(function(stream) {
+    addVideoStream(myVideo, stream);
+    myPeer.on('call', function(call) {
+        call.answer(stream);
+        const video = document.createElement('video');
+        call.on('stream', function(userVideoStream) {
+            addVideoStream(video, userVideoStream);
+        });
+    });
+    socket.on('user-connected', function(userId) {
+        connectToNewUser(userId, stream);
+    });
+});
+
+socket.on('user-disconnected', function(userId) {
+    console.log(userId);
+    if(peers[userId]) peers[userId].close();
+});
+
+myPeer.on('open', function(id) {
+    socket.emit('join-room', ROOM_ID, id);
+});
+
+function connectToNewUser(userId, stream) {
+    const call = myPeer.call(userId, stream);
+    const video = document.createElement('video');
+    call.on('stream', function(userVideoStream) {
+        addVideoStream(video, userVideoStream);
+    });
+    call.on('close', function() {
+        video.remove();
+    });
+    peers[userId] = call;
+}
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    });
+    videoGrid.append(video);
+}
---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { v4: uuidv4 } = require('uuid');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get('/:room', function(req, res) {
+    res.render('room', { roomId: req.params.room });
+});
+
+io.on('connection', function(socket) {
+    socket.on('join-room', function(roomId, userId) {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected', userId);
+        socket.on('disconnect', function() {
+            socket.to(roomId).broadcast.emit('user-disconnected', userId);
+        });
+    });
+});
+
+server.listen(3000);
